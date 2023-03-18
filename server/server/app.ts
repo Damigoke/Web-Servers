f8699985c43b2442f2220402acd8a6b13d82fc93
Damilola Adegoke <d.botanist20@gmail.com>
2023-03-18 17:42:32 +0100

    Task done

--- server/server/app.ts
+++ server/server/app.ts
@@ -5,7 +5,7 @@ import http, { IncomingMessage, Server, ServerResponse } from "http";
 /*
 implement your server code here
 */
-import fs, { writeFileSync } from "fs";
+import fs, { writeFileSync, existsSync} from "fs";
 // import { parse } from 'path';
 // import { buffer } from 'stream/consumers';
 // let path = "./server/inputs/database.json"
@@ -23,8 +23,8 @@ if(fs.existsSync("./server/inputs/database.json")){
 const dateNow = new Date()
 interface fileInfo {
   organization: string,
-  createdAt: string,
-  updatedAt: string,
+  createdAt: Date,
+  updatedAt: Date,
   products: string[],
   marketValue: string,
   address: string,
@@ -37,25 +37,23 @@ interface fileInfo {
 
 const server: Server = http.createServer((req: IncomingMessage, res: ServerResponse) => {
     if (req.method === "GET") {
-      if(req.url === "/api") {
+      if(req.url === "/") {
         res.writeHead(200, {'Content-Type': 'application/json'})
-        res.end(JSON.stringify({parse}));
-      }else{
-        fs.writeFileSync("./server/inputs/database.json", "uft8")
+        res.end(JSON.stringify(parse));
       }
     }
     if (req.method === "POST") {
-      if(req.url === "/api/data") {
+      if(req.url === "/data") {
         let result = "";
       req.on('data', (chunk) => {
         result += chunk;
         result;
-        
+      })
         req.on('end', () => {
-        const data = JSON.parse(result)
+        const data: fileInfo = JSON.parse(result)
         data.createdAt = dateNow;
-        const numId = data.id;
-        const id = numId;
+        const totalNoOfEmployee = data.employees.length;
+        data.noOfEmployees = totalNoOfEmployee;
         const name = data.organization
         // if(name){
         //   let id = parse && parse.length === 0 ? 1 : parse[parse.length - 1].id;
@@ -64,53 +62,59 @@ const server: Server = http.createServer((req: IncomingMessage, res: ServerRespo
         if (name) {
           let id = 1;
           if (parse && parse.length > 0) {
-            id = parse[parse.length - 1].id;
+            id = parse[parse.length - 1].id + 1;
           }
-        parse.push(data);
+          data.id = id
+          parse.push(data);
         }
         const jsonString = JSON.stringify(parse, null, 2);
         fs.writeFileSync('./server/inputs/database.json', jsonString);
         res.writeHead(200, {'Content-Type': 'application/json'})
-        res.end("Database Created");
+        res.end(JSON.stringify({parse}));
       })
         
-      })
-     }
+      }
     }
     if(req.method === 'PUT') {
+      if(req.url && req.url.includes('/data/')) {
+          const id = parseInt(req.url.split("/")[2])
+        let result = "";
         req.on('data', data => {
-          let parseData = JSON.parse(data);
+          result += data
+          result;
+        })
+        
+        req.on('end', () => {
+            let parseData: fileInfo = JSON.parse(result);
           parseData.updatedAt = dateNow;
-          const id = parseData.id;
-          data.forEach((value:fileInfo, index:number) => {
-            if(value.id === id){
-              data[index] = parseData;
-            }
-          })
-          const jsonString = JSON.stringify(parseData, null, 2);
+          parseData.noOfEmployees = parseData.employees.length;
+          const newParse = parse.findIndex(item => item.id === id)
+          if(newParse !== -1) {
+             parse[newParse] = {...parse[newParse], ...parseData }
+     
+             }
+     
+        const jsonString = JSON.stringify(parse, null, 2);
         fs.writeFileSync('./server/inputs/database.json', jsonString);
         res.writeHead(200, {'Content-Type': 'application/json'})
-          res.end('Database updated');
-        })
+        res.end(JSON.stringify({ parse }));
+          })
+       }
       
-    }
+   }
     if (req.method === "DELETE") {
-      req.on('data', data => {
-        let parseData = JSON.parse(data);
-        const id = parseData.id;
-        let database = fs.readFileSync('./server/inputs/database.json', 'utf-8');
-        let jsonData = JSON.parse(database);
-        
-        if (!Array.isArray(jsonData)) {
-          jsonData = [jsonData];
-        }
-        
-        let filteredData = jsonData.filter((value:fileInfo, index:number) => value.id !== id);
-        fs.writeFileSync('./server/inputs/database.json', JSON.stringify(filteredData), 'utf-8');
-        res.writeHead(200, {'Content-Type': 'application/json'})
-        res.end('Database Deleted');
-      });
-    }
-});
+      if(req.url && req.url.includes('/data/')) {
+         const id = parseInt(req.url.split("/")[2])
+         const newParse = parse.findIndex(item => item.id === id)
+          if(newParse !== -1) {
+              parse.splice(newParse, 1)
+            const jsonString = JSON.stringify(parse, null, 2)
+            fs.writeFileSync('./server/inputs/database.json', jsonString)
+             }
+          res.writeHead(200, {'Content-Type': 'application/json'})
+          res.end(JSON.stringify({ parse }));
+        };
+      }
+    })
 
 server.listen(3005);
